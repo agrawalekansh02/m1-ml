b9b9347d3978c8f6e0aaa7664b5fcce526b309be
Ekansh Agrawal <agrawalekansh@gmail.com>
2021-12-27 01:31:40 -0800

    first successful cnn test

---
+++ mnist_cnn.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.model_selection import GridSearchCV
+import keras
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+
+# data
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+img_rows, img_cols = 28, 28
+num_classes = 10
+X_train = X_train[:40000]
+Y_train = Y_train[:40000]
+X_train = X_train.reshape(X_train.shape[0], img_rows*img_cols)
+X_test = X_test.reshape(X_test.shape[0], img_rows*img_cols)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+Y_train = keras.utils.np_utils.to_categorical(Y_train, num_classes)
+Y_test = keras.utils.np_utils.to_categorical(Y_test, num_classes)
+
+# reshape data, depending on Keras backend
+if keras.backend.image_data_format() == 'channels_first':
+    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+# model
+def CNN_var(optimizer, activation):
+  model = Sequential()
+  model.add(Conv2D(10, kernel_size=(5, 5),
+                 activation=activation,
+                 input_shape=input_shape))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Conv2D(10, (5, 5), activation=activation))
+  model.add(Dropout(0.5))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Flatten())
+  model.add(Dense(20*4*4, activation=activation))
+  model.add(Dropout(0.5))
+  model.add(Dense(num_classes, activation='softmax'))
+  model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=optimizer,
+              metrics=['accuracy'])
+  return model
+  
+# training
+batch_size = 128
+epochs = 5
+model_gridsearch = KerasClassifier(build_fn=CNN_var, 
+                        epochs=epochs, 
+                        batch_size=batch_size,
+                        verbose=1)
+
+# scikit search module
+activation = ['sigmoid', 'relu']
+optimizer = ['SGD', 'Adam','Nadam']
+param_grid = dict(activation=activation, optimizer=optimizer)
+grid = GridSearchCV(estimator=model_gridsearch, param_grid=param_grid, n_jobs=1, cv=2)
+grid_result = grid.fit(X_train, Y_train)
+
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
